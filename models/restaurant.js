001d25d7b9e31926ba5c442163435f17f1a8db23
Ben Manning <bmanning@me.com>
2022-08-01 10:43:00 -0400

    successful db query for restaurants

---
+++ models/restaurant.js
@@ -0,0 +1,11 @@
+const { Schema } = require('mongoose')
+
+const restaurantSchema = new Schema({
+    name: { type: String, required: true },
+    location: { type: String, required: true},
+    photo_url: { type: String, required: true }
+}, 
+{ timestamps: true }
+)
+
+module.exports = restaurantSchema
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
 
-const app = express() 
+const app = express()
+
+const { Restaurant } = require('./models')
 
 // MIDDLEWARE
 app.use(express.static(`${__dirname}/client/build`))
 
 // ROUTES
-app.get('/restaurants', (req, res) => {
-  res.send('root route hit')
+app.get('/restaurants', async (req, res) => {
+  let restaurants = await Restaurant.find({})
+  res.send(restaurants)
 })
 
 app.get('/*', (req, res) => {
