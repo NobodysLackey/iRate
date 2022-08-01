9803e43d1b4da0828becb8a6ed871b3fafac7d2a
Ben Manning <bmanning@me.com>
2022-08-01 11:16:12 -0400

    got the POST to restaurants route working

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const app = express()
 const { Restaurant } = require('./models')
 
 // MIDDLEWARE
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(`${__dirname}/client/build`))
+
 
 // ROUTES
 app.get('/restaurants', async (req, res) => {
   let restaurants = await Restaurant.find({})
   res.send(restaurants)
+})
+
+app.post('/restaurants', async (req, res) => {
+    let createdRestaurant = await Restaurant.create(req.body)
+    res.send(createdRestaurant)
 })
 
 app.get('/*', (req, res) => {
