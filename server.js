b040b09f7ba79d5fd39a70f57686674802e17742
Ben Manning <bmanning@me.com>
2022-08-01 09:48:55 -0400

    building out initial heroku connection

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const PORT = process.env.PORT || 3001
 
 const app = express() 
 
-// routes 
+// MIDDLEWARE
+app.use(express.static(`${__dirname}/client/build`))
 
+// ROUTES
 app.get('/', (req, res) => {
   res.send('root route hit')
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
 
 app.listen(PORT, () => 
   console.log('server is running at PORT', PORT)
-
 )
