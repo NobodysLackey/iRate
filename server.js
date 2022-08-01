639f51d77414dfa4f966e2077fa7012e4d7416db
Ben Manning <bmanning@me.com>
2022-08-01 09:31:08 -0400

    fixing merge conflict for the second time

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 
+<<<<<<< HEAD
 const app = express() 
 
 // routes 
@@ -14,3 +15,15 @@ app.listen(PORT, () =>
   console.log('server is running at PORT', PORT)
 
 )
+=======
+const app = express()
+
+// simple testing route
+app.get('/', (req, res) => {
+    res.send('I have hit the root route')
+})
+
+app.listen(PORT, () => {
+    console.log(`I am currently running at PORT:`, PORT)
+})
+>>>>>>> 5a9bd12 (built basic server boilerplate)
