9ad4a8fbf716d39dc019a2a9f9105b437748d247
Ben Manning <bmanning@me.com>
2022-08-01 13:52:52 -0400

    added the get all route for reviews

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
-const { restaurants } = require('../controllers')
+const { restaurants, reviews } = require('../controllers')
 const router = Router();
 
+// restaurant routes
 router.get('/restaurants', restaurants.getAllRestaurants)
 router.post('/restaurants', restaurants.createRestaurant)
 router.get('/restaurants/:id', restaurants.getOneRestaurant)
+
+// review routes
+router.get('/reviews', reviews.getAllReviews)
 
 module.exports = router;
