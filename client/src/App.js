223711d575af92dc8e5c96c82682e426256b3e2d
Michael Lackey <michael.g.lackey@gmail.com>
2022-08-02 11:27:27 -0400

    successfully retrieve data from backend

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,22 @@ import ReviewDetails from './pages/ReviewDetails'
 import RestaurantDetails from './pages/RestaurantDetails'
 import ReviewForm from './pages/ReviewForm'
 import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { BASE_URL } from './globals'
 import './App.css'
 
 const App = () => {
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    const getRestaurants = async () => {
+      const res = await axios.get(`${BASE_URL}/api/restaurants`)
+      setRestaurants(res.data)
+    }
+    getRestaurants()
+  }, [])
+
   return (
     <div>
       <header>
