0691026165cf16f2db42a10605334a0f300030cc
Michael Lackey <michael.g.lackey@gmail.com>
2022-08-03 16:28:18 -0400

    style review cards

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -2,12 +2,16 @@ const ReviewCard = ({ review, editReview, deleteReview, index }) => {
 
   return (
     <div className="review-card">
-      <h4>{review.title}</h4>
+      <div className="review-top">
+        <h4>{review.title}</h4>
+        <button onClick={() => deleteReview(review._id, index)}>X</button>
+        <h3>{review.rating} / 5</h3>
+      </div>
       <p>{review.body}</p>
-      <h4>{review.name}</h4>
-      <h3>{review.rating}</h3>
-      <button onClick={() => editReview(review, index)}>Edit</button>
-      <button onClick={() => deleteReview(review._id, index)}>X</button>
+      <div className="review-bottom">
+        <h4>{review.name}</h4>
+        <button onClick={() => editReview(review, index)}>Edit</button>
+      </div>
     </div>
   )
 }
