436b5ee3c29e89ee9043f606a0e530a72a3dbeaf
Michael Lackey <michael.g.lackey@gmail.com>
2022-08-03 16:44:51 -0400

    style emoji rating

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,11 +1,43 @@
 const ReviewCard = ({ review, editReview, deleteReview, index }) => {
 
+  let emojiRating
+  let grayEmojis
+
+  const getEmojiRating = () => {
+    switch(review.rating) {
+      case 5:
+        emojiRating = "🤤🤤🤤🤤🤤"
+        grayEmojis = ""
+        break
+      case 4:
+        emojiRating = "🙂🙂🙂🙂"
+        grayEmojis = "😐"
+        break
+      case 3:
+        emojiRating = "😠😠😠"
+        grayEmojis="😐😐"
+        break
+      case 2:
+        emojiRating = "😡😡"
+        grayEmojis="😐😐😐"
+        break
+      case 1:
+        emojiRating = "🤬"
+        grayEmojis="😐😐😐😐"
+        break
+      default:
+        emojiRating = "🤬"
+        grayEmojis="😐😐😐😐"
+    }
+  }
+  getEmojiRating()
+
   return (
     <div className="review-card">
       <div className="review-top">
         <h4>{review.title}</h4>
         <button onClick={() => deleteReview(review._id, index)}>X</button>
-        <h3>{review.rating} / 5</h3>
+        <h3>{emojiRating}<span>{grayEmojis}</span></h3>
       </div>
       <p>{review.body}</p>
       <div className="review-bottom">
